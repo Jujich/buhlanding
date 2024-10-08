8327cae2e134e159945ba1fd6bdb665f866cd3f8
Jujich <kuzma.korolev.94@mail.ru>
2024-10-08 23:58:42 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <main>
+
+  </main>
+
+  <footer>
+
+  </footer>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
---
+++ src/components/ConsultFormPopup.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "ConsultFormPopup"
+}
+</script>
+
+<template>
+  <form class="popup-form">
+
+  </form>
+</template>
+
+<style scoped>
+.popup-form {
+  width: 370px;
+  height: 80px;
+}
+</style>
